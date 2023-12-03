5488b931ce73afc9812a1ae46c0ce271dab81fc4
ana117 <63540951+ana117@users.noreply.github.com>
2023-12-03 16:35:42 +0700

    Add all pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
 	return render_template("index.html")
+
+@app.route("/search")
+def search():
+	query = request.args.get("q")
+	return render_template("search.html", query=query)
+
+@app.route("/document/<id>")
+def document(id):
+	return render_template(
+		"document.html",
+		doc_title="Document " + id,
+		doc_text="This is the text of document " + id)
 
 if __name__ == '__main__':
 	app.run(debug=True)
