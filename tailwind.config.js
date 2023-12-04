f5f626e7094bcdfc81011748de6f3b7492ce20c1
ana117 <63540951+ana117@users.noreply.github.com>
2023-12-04 09:09:02 +0700

    Improve styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./templates/**/*.html",
     "./static/src/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': 'var(--text)',
+        'background': 'var(--background)',
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+        'accent': 'var(--accent)',
+       },
+    },
   },
   plugins: [],
 }
